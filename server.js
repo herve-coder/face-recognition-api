7edb428d73c2fdcd0bbed73dd7a6bf405da89ada
TRAORE Hervé <htraore1976@gmail.com>
2019-09-25 23:54:02 +0000

    Adding port

--- server.js
+++ server.js
@@ -37,4 +37,7 @@ app.post("/imageurl", (req, res) => {
 
 app.post("/register", registerHandler(bcrypt, db));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App is listening to port ${process.env.PORT || 3000}`);
+  
+});
